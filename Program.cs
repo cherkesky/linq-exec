de2cad6bc4beb77f88e05a660cda4ceb3fc24321
Cherkesky <guy@cherkesky.com>
2020-05-06 18:10:03 -0500

    How much money have we made?

--- Program.cs
+++ Program.cs
@@ -17,9 +17,9 @@ namespace linq
                                     select fruit;
       System.Console.WriteLine("All the fruits that start with L");
       foreach (string Lfruit in LFruits)
-    {
+      {
         System.Console.WriteLine(Lfruit);
-    }
+      }
 
       // Which of the following numbers are multiples of 4 or 6
       List<int> numbers = new List<int>()
@@ -31,9 +31,9 @@ namespace linq
       System.Console.WriteLine("All the numbers that are multiplies of 4 or 6");
 
       foreach (int fourSixMultiple in fourSixMultiples)
-    {
+      {
         System.Console.WriteLine(fourSixMultiple);
-    }
+      }
 
       // Order these student names alphabetically, in descending order (Z to A)
       List<string> names = new List<string>()
@@ -47,9 +47,9 @@ namespace linq
       System.Console.WriteLine("Order these student names alphabetically, in descending order (Z to A)");
       List<string> descend = names.OrderByDescending(people => people).ToList();
       foreach (string descendNames in descend)
-    {
+      {
         System.Console.WriteLine(descendNames);
-    }
+      }
 
       // Build a collection of these numbers sorted in ascending order
       List<int> numbers2 = new List<int>()
@@ -58,11 +58,29 @@ namespace linq
     };
       System.Console.WriteLine("Build a collection of these numbers sorted in ascending order");
 
-        List<int> ascends = numbers2.OrderBy(nums2 => nums2).ToList();
-        foreach (int ascend in ascends)
-    {
+      List<int> ascends = numbers2.OrderBy(nums2 => nums2).ToList();
+      foreach (int ascend in ascends)
+      {
         System.Console.WriteLine(ascend);
-    }
+      }
+      // Output how many numbers are in this list
+      List<int> numbers3 = new List<int>()
+    {
+        15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+    };
+      System.Console.WriteLine("Output how many numbers are in this list");
+
+        int counter=numbers3.Count();
+        System.Console.WriteLine(counter);
+
+    // How much money have we made?
+    List<double> purchases = new List<double>()
+    {
+        2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+    };
+        System.Console.WriteLine("How much money have we made?");
+      double counter2=purchases.Sum();
+        System.Console.WriteLine(counter2);
 
 
     }
