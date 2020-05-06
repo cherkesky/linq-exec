303c4979042522fc9b003b7d92dc1c12b9276a4a
Cherkesky <guy@cherkesky.com>
2020-05-06 17:46:54 -0500

    Which of the following numbers are multiples of 4 or 6

--- Program.cs
+++ Program.cs
@@ -12,13 +12,28 @@ namespace linq
       // Find the words in the collection that start with the letter 'L'
       List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
 
-     IEnumerable<string> LFruits = from fruit in fruits where fruit[0] is 'L'
-     select fruit;
-        
-    foreach (string Lfruit in LFruits){
+      IEnumerable<string> LFruits = from fruit in fruits
+                                    where fruit[0] is 'L'
+                                    select fruit;
+        System.Console.WriteLine("All the fruits that start with L");
+      foreach (string Lfruit in LFruits)
+      {
         System.Console.WriteLine(Lfruit);
-    }
+      }
 
+      // Which of the following numbers are multiples of 4 or 6
+      List<int> numbers = new List<int>()
+      {
+            15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+      };
+
+      IEnumerable<int> fourSixMultiples = numbers.Where(n=> n%4==0 || n%6==0);
+    System.Console.WriteLine("All the numbers that are multiplies of 4 or 6");
+
+      foreach ( int fourSixMultiple in fourSixMultiples)
+      {
+        System.Console.WriteLine(fourSixMultiple);
+      }
     }
   }
 }
